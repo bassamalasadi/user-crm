bcb2f2dd735b08a11555f6ccbd89c8f53eeff1e1
bassamalasadi <bassamalasadi@gmail.com>
2021-04-11 09:03:42 +0300

    final update

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
-import './App.css';
+import React from "react";
+import './App.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import ListUsers from './Components/ListUsers'
+
 
 function App() {
   return (
-    <div className="App">
-      Hello world
+    <div>
+        <ListUsers />
     </div>
   );
 }
